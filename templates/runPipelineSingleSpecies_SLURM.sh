c5f0d4ce412852104bcdff8b3efb2f9894e01670
Charles Girardot <girardot@embl.de>
2018-08-07 11:28:07 +0200

    create an EMBL dir to store the EMBL specific config/data

--- templates/runPipelineSingleSpecies_SLURM.sh
+++
@@ -1,1 +0,0 @@
-snakemake  --jobs 100 --local-cores 8 --restart-times 1 --cluster 'sbatch -N 1 -n {cluster.n}  -t {cluster.time} --mem={cluster.mem} --output={cluster.output} --error={cluster.error}  --mail-type=FAIL'  --cluster-config cluster.yaml --use-conda meta qc filter map extract
